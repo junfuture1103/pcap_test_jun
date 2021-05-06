87c63028e72d5fae18dc150a4d8b302c7c39df34
junfuture1103 <junhak1103@naver.com>
2021-05-06 19:51:18 +0900

    ADD DATA CAPTURE

--- pcap-test.cpp
+++ pcap-test.cpp
@@ -36,11 +36,11 @@ int main(int argc, char* argv[]) {
 
 	while (true) {
 		struct pcap_pkthdr* header;
-
         libnet_ethernet_hdr *eth_hdr;
         libnet_tcp_hdr *tcp_hdr;
 
         const u_char* packet;
+        int data_size;
 
 		int res = pcap_next_ex(pcap, &header, &packet);
         if (res == 0) continue;
@@ -48,14 +48,23 @@ int main(int argc, char* argv[]) {
 			printf("pcap_next_ex return %d(%s)\n", res, pcap_geterr(pcap));
 			break;
         }
+        //hdr
+        eth_hdr = (libnet_ethernet_hdr*)packet;
+        libnet_ipv4_hdr *ip_hdr_v4 = (libnet_ipv4_hdr*)(packet + sizeof(libnet_ethernet_hdr));
 
+        if (ntohs(eth_hdr->ether_type) != ETHERTYPE_IP){
+            //printf("PASS! type : %d\n", ntohs(eth_hdr->ether_type));
+            continue;
+        }
+        if(ip_hdr_v4->ip_p != IPPROTO_TCP){
+            //printf("PASS! protocol : %d\n", ip_hdr_v4->ip_p);
+            continue;
+        }
 
         printf("[%d] %u bytes captured\n",index, header->caplen);
-        //ethernet hdr
-        eth_hdr = (libnet_ethernet_hdr*)packet;
 
         //ethernet hdr source_mac
-        printf("sour MAC : ");
+        printf("\nsour MAC : ");
         for (int i = 0; i<ETHER_ADDR_LEN; i++){
             if (i == ETHER_ADDR_LEN-1){
                 printf("0x%02x", eth_hdr->ether_shost[i]);
@@ -64,9 +73,10 @@ int main(int argc, char* argv[]) {
                 printf("0x%02x:", eth_hdr->ether_shost[i]);
             }
         }
+
+        //ethernet hdr dest_mac
         printf("\n");
         printf("dest MAC : ");
-        //ethernet hdr dest_mac
         for (int i = 0; i<ETHER_ADDR_LEN; i++){
             if (i == ETHER_ADDR_LEN-1){
                 printf("0x%02x", eth_hdr->ether_shost[i]);
@@ -76,40 +86,38 @@ int main(int argc, char* argv[]) {
             }
         }
         printf("\n");
-        packet += 14;
-        if (eth_hdr->ether_type == 0x0008){
-            libnet_ipv4_hdr *ip_hdr_v4 = (libnet_ipv4_hdr*)packet;
 
-            //IP hdr source_ip
-            printf("sour IP : ");
-            printf("%s\n", inet_ntoa(ip_hdr_v4->ip_src));
+        //IP hdr source_ip
+        printf("sour IP : ");
+        printf("%s\n", inet_ntoa(ip_hdr_v4->ip_src));
 
-            //IP hdr destination_ip
-            printf("dest IP : ");
-            printf("%s", inet_ntoa(ip_hdr_v4->ip_dst));
+        //IP hdr destination_ip
+        printf("dest IP : ");
+        printf("%s", inet_ntoa(ip_hdr_v4->ip_dst));
 
-            if (ip_hdr_v4->ip_p == 6){
-                packet += (ip_hdr_v4->ip_hl*4);
-                tcp_hdr = (libnet_tcp_hdr *)packet;
+        packet = packet + sizeof(libnet_ethernet_hdr) + (ip_hdr_v4->ip_hl*4);
+        tcp_hdr = (libnet_tcp_hdr *)packet;
 
-                //TCP hdr
-                printf("\nsour port : %d\n", ntohs(tcp_hdr->th_sport));
-                printf("dest port : %d", ntohs(tcp_hdr->th_dport));
+        //TCP hdr
+        printf("\nsour port : %d\n", ntohs(tcp_hdr->th_sport));
+        printf("dest port : %d", ntohs(tcp_hdr->th_dport));
 
+        //DATA
+        printf("\nip_hdr_v4 : %d\n",ntohs(ip_hdr_v4->ip_len));
+        data_size = ntohs(ip_hdr_v4->ip_len) - ip_hdr_v4->ip_hl*4 - tcp_hdr->th_off*4;
+        printf("\nDATA size : %d", data_size);
+
+        if (data_size != 0){
+            if (data_size > 16){
+                data_size = 16;
             }
-            else{
-                printf("This is not TCP / protocol : %d", ip_hdr_v4->ip_p);
-                //printing end..
-                index++;
-                printf("\n");
-                continue;
+            packet += tcp_hdr->th_off*4;
+            printf("\nDATA : ");
+            for (int i = 0; i<data_size; i++){
+                printf("%0x02x ", *(packet+i));
             }
-        }else {
-            printf("This is not IPv4");
-            //printing end..
-            index++;
-            printf("\n");
-            continue;
+        }else{
+            printf("\nData size is 0\n");
         }
         //printing end..
         index++;
